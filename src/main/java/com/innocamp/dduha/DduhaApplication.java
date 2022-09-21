d8041c679ff53b519cfb7096fed364573069279f
sooominnn <lsm980830@gmail.com>
2022-09-21 11:41:15 +0900

    first commit

---
+++ src/main/java/com/innocamp/dduha/DduhaApplication.java
@@ -0,0 +1,13 @@
+package com.innocamp.dduha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DduhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DduhaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innocamp/dduha/DduhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innocamp.dduha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DduhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
