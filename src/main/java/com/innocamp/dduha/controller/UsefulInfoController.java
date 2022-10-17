4fa513a811607c06edf7b5b0f38bf3f4554f073f
dajeong09 <chic519@gmail.com>
2022-10-17 18:47:24 +0900

    fix : 유용한 정보 api 카테고리 별 요청

--- src/main/java/com/innocamp/dduha/controller/UsefulInfoController.java
+++ src/main/java/com/innocamp/dduha/controller/UsefulInfoController.java
@@ -4,6 +4,7 @@ import com.innocamp.dduha.dto.ResponseDto;
 import com.innocamp.dduha.service.UsefulInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,7 @@ public class UsefulInfoController {
     private final UsefulInfoService usefulInfoService;
 
     @GetMapping("/information")
-    public ResponseDto<?> getUsefulInfo() {
-        return usefulInfoService.getUsefulInfo();
+    public ResponseDto<?> getUsefulInfo(@RequestParam(value="category") String category) {
+        return usefulInfoService.getUsefulInfo(category);
     }
 }
